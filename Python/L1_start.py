7f183d50ee6c8cf4f1f64bf9ccc6bfe3b59cfe69
salima5214 <mingming5214@gmail.com>
2021-12-14 02:40:08 -0500

    add L1 L2

---
+++ Python/L1_start.py
@@ -0,0 +1,5 @@
+# download Python 3
+# download Visual Studio Code
+# L1: https://reurl.cc/43AgvX
+print("Hello Python")
+
---
+++ Python/L2_datatype.py
@@ -0,0 +1,6 @@
+# L2: https://reurl.cc/oxnD6V
+
+myList = [3, 4, 5]
+myTuple = (3, 4, 5)
+mySet = {"Hello", "World"}
+myDictionary = {"Pass": 100, "Fail": 0}
